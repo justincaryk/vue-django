ba004c12148e3d550e167553357387ee4ca39d68
Justin Caryk <cary9983@gmail.com>
2024-08-08 14:37:06 -0700

    initial commit

---
+++ frontend/src/components/Increment.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, onMounted, type Ref } from 'vue'
+
+// reactive state
+const count = ref<number>(0)
+
+// functions that mutate state and trigger updates
+function increment() {
+  count.value++
+}
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
+</script>
+
+<template>
+    <div>Count is at {{  count }}</div>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
---
+++ frontend/src/views/PlaygroundView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Increment from '../components/Increment.vue'
+</script>
+
+<template>
+  <main>
+    <Increment />
+  </main>
+</template>
